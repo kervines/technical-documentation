47880e59759616215452f2fcff01b19a69632d7f
kervines <kervinzero@gmail.com>
2023-09-06 23:20:19 -0300

    alterações finais

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
 const navBar = document.querySelector('#navbar');
-const btnClose = document.querySelector('.close');
-const btnOpen = document.querySelector('.open');
+const btnOpenClose = document.querySelector('.open-close');
+const container = document.querySelector('.container');
+btnOpenClose.style.left = '0px';
+navBar.style.display = 'none';
+
+function btnClose() {
+  btnOpenClose.style.left = '270px';
+  btnOpenClose.innerHTML = '❌';
+  navBar.style.display = 'block';
+}
+function btnOpen() {
+  btnOpenClose.style.left = '0px';
+  btnOpenClose.innerHTML = '➡️';
+}
+function moveIn() {
+  container.classList.remove('move-out');
+  container.classList.add('move-in');
+}
+function moveOut() {
+  container.classList.remove('move-in');
+  container.classList.add('move-out');
+}
+
+btnOpenClose.addEventListener('click', () => {
+  if (navBar.classList.contains('hide')) {
+    navBar.classList.remove('hide');
+    navBar.classList.add('show');
+    btnClose();
+    moveOut();
+  } else {
+    navBar.classList.remove('show');
+    navBar.classList.add('hide');
+    btnOpen();
+    moveIn();
+  }
+});
