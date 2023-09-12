502540c226feb00aeca90ce310b213b950350bf4
kervines <kervinzero@gmail.com>
2023-09-11 22:49:12 -0300

    finalizado menu mobile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const buttonOpen = document.querySelector('.open');
+const buttonClose = document.querySelector('.close');
 const navBarElement = document.querySelector('#navbar');
 
 buttonOpen.addEventListener('click', () => {
+  navBarElement.classList.toggle('hide');
+});
+buttonClose.addEventListener('click', () => {
   navBarElement.classList.toggle('hide');
 });
