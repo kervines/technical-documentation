373ee46bf24e68d9e298695e50e8b14d265c9649
kervines <kervinzero@gmail.com>
2023-09-11 23:23:58 -0300

    finalizado implementação js do menu mobile

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 const buttonOpen = document.querySelector('.open');
 const buttonClose = document.querySelector('.close');
 const navBarElement = document.querySelector('#navbar');
+const links = document.querySelectorAll('#navbar ul li a');
+const buttonReturn = document.querySelector('.top-return');
 
 buttonOpen.addEventListener('click', () => {
   navBarElement.classList.toggle('hide');
 });
 buttonClose.addEventListener('click', () => {
   navBarElement.classList.toggle('hide');
+});
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    navBarElement.classList.toggle('hide');
+  });
+});
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    buttonReturn.style.opacity = '.5';
+  } else {
+    buttonReturn.style.opacity = '0';
+  }
 });
