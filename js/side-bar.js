199d3de139025664664c0c6c244cf20ab6927704
kervines <kervinzero@gmail.com>
2024-01-04 23:43:21 -0300

    feat - style - criado um arquivo para o `side-bar.js` e o `script.js` recebe os imports dos outros arquivos js.

---
+++ js/side-bar.js
@@ -0,0 +1,36 @@
+export default function initSideBar() {
+  const buttonOpen = document.querySelector('.open');
+  const buttonClose = document.querySelector('.close');
+  const navBarElement = document.querySelector('#navbar');
+  const links = document.querySelectorAll('#navbar ul li a');
+  const buttonReturn = document.querySelector('.top-return');
+
+  buttonOpen.addEventListener('click', () => {
+    navBarElement.classList.toggle('hide');
+  });
+  buttonClose.addEventListener('click', () => {
+    navBarElement.classList.toggle('hide');
+  });
+
+  links.forEach((link) => {
+    link.addEventListener('click', () => {
+      if (window.innerWidth <= 420 || window.innerWidth <= 720) {
+        navBarElement.classList.toggle('hide');
+      }
+    });
+  });
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 720 && navBarElement.classList.contains('hide')) {
+      navBarElement.classList.remove('hide');
+    }
+  });
+
+  document.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      buttonReturn.style.opacity = '.5';
+    } else {
+      buttonReturn.style.opacity = '0';
+    }
+  });
+}
